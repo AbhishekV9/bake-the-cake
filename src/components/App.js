0642e50db56aed7b0db7013fe4cac8dbf8de2c3e
Abhishek Verma <avishekv444@gmail.com>
2021-10-13 00:36:12 +0530

    added provided styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
-
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div>Hellow</div>
+    <div className="bg-dark h-100 p-4">
+      <div className="container-fluid">
+
+      </div>
+    </div>
   );
 }
 
