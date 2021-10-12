c602b8a26b5e7b77d6dd603b18ca92c49bf5fd3d
Abhishek Verma <avishekv444@gmail.com>
2021-10-13 01:45:22 +0530

    showing two cakes

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,9 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="bg-dark vh-100 p-4">
-      <div className="container-fluid">
-          <Cake />
+      <div className="container-fluid d-flex justify-content-between">
+          <Cake id={1}/>
+          <Cake id={2}/>
       </div>
     </div>
   );
--- src/components/Cake.js
+++ src/components/Cake.js
@@ -1,11 +1,12 @@
 
-function cake (){
+function cake (props){
+    console.log(props)
     return(
         <div className="">
             <div className="box-0 ms-4"></div>
             <div className="box-1 ms-3"></div> 
             <div className="box-2 ms-2"></div>
-            <div className="box-3"><p className="fs-5 text-center pt-1 fw-bold text-white">1</p></div>
+            <div className="box-3"><p className="fs-5 text-center pt-1 fw-bold text-white">{props.id}</p></div>
         </div>
     )
 }
