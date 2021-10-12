b6dd78c29aaae057ef6c14bda70c45389799d032
Abhishek Verma <avishekv444@gmail.com>
2021-10-13 01:39:20 +0530

    showing single cake

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
+import Cake from "./Cake";
+
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="bg-dark h-100 p-4">
+    <div className="bg-dark vh-100 p-4">
       <div className="container-fluid">
-
+          <Cake />
       </div>
     </div>
   );
---
+++ src/components/Cake.js
@@ -0,0 +1,13 @@
+
+function cake (){
+    return(
+        <div className="">
+            <div className="box-0 ms-4"></div>
+            <div className="box-1 ms-3"></div> 
+            <div className="box-2 ms-2"></div>
+            <div className="box-3"><p className="fs-5 text-center pt-1 fw-bold text-white">1</p></div>
+        </div>
+    )
+}
+
+export default cake;
